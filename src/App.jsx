378e46e6525dd64d8364bacecb82d98f9917c557
Azeem Khan <azeemkhanreal@gmail.com>
2022-10-09 16:03:55 +0530

    add header

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Homepage from "./pages/homepage/homepage.component";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import ShopPage from "./pages/shop/shop.component";
 import "./app.styles.scss";
+import Header from "./components/header/header.component";
 
 const Hats = () => {
   return <h1>Hats</h1>;
@@ -11,6 +12,7 @@ function App() {
   return (
     <div className="App">
       <Router>
+        <Header />
         <Routes>
           <Route path="/" element={<Homepage />} />
           <Route path="/hats" element={<Hats />} />
